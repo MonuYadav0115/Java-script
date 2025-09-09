b5d79b2e4d2076b3dd7252fc79894c7c04f34190
Monu Yadav <118626203+MonuYadav0115@users.noreply.github.com>
2025-09-09 15:49:57 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 12345
+let accountEmail = "monuy1234@gmail.com"
+var accountpassword = "12345678"
+accountcity = "Hyderabad"
+// accountId = 2
+accountEmail ="tom123@gmail.co"
+accountcity ="bangaluru"
+accountpassword="11223344"
+
+console.log(accountId);
+
+console.log([accountId,accountEmail,accountpassword,accountcity])
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,1 @@
-console.log("Monu")
+console.log("Monu Yadav")
