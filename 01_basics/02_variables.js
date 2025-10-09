03f872a454efe82a1da1935275dba0f880622e8f
Monu Yadav <118626203+MonuYadav0115@users.noreply.github.com>
2025-10-09 06:13:16 +0000

    container config done

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,16 @@
+// const accountId =12345
+// accountId=54321
+// console.log(accountId);
+
+
+let accountEmail="monu@google.com"
+var accountpassword="12345678"
+accountcity="Hyderabad"
+
+accountEmail="tom@gmail.com"
+accountpassword="87654321"
+accountcity="Benglore"
+
+// accountState;  ----- output undefined 
+
+console.table([accountEmail,accountpassword,accountcity, accountstate])
