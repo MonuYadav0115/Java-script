e65954e0e991159a49aca83bf05e1c4fb76b96a6
Monu Yadav <118626203+MonuYadav0115@users.noreply.github.com>
2025-10-09 07:58:40 +0000

    datatype

---
+++ 01_basics/03_datatype.js
@@ -0,0 +1,20 @@
+// Data Types conversion
+
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+
+let score1="33"
+let valueInNumber1 = Number(score1);
+console.log(typeof valueInNumber1);
+
+
+let score2 = "33abc"
+let valueInNumber2 = Number(score2)
+console.log(valueInNumber2)
+console.log(typeof valueInNumber2)
