82980ec078d8479ba3dbdefc5879250b1414a50c
Monu Yadav <118626203+MonuYadav0115@users.noreply.github.com>
2025-10-14 05:54:38 +0000

    conversion type

---
+++ 01_basics/04_conversion.js
@@ -0,0 +1,26 @@
+let isLoggedIn = 1
+
+let boolenisloggedIn = Boolean(isLoggedIn)
+
+// console.log(boolenisloggedIn)
+
+let isnotLoggedIn = 0
+
+let booleanisnotloggedIn = Boolean(isnotLoggedIn)
+
+// console.log(booleanisnotloggedIn)
+
+// Notes if you cheak these type of value then output 
+// 1 => true 
+//  0 => False
+//  "" => false
+//  "monu" => true
+
+let someNumber = 33
+
+let Stringnumber = String(someNumber)
+
+console.log(Stringnumber);
+console.log(typeof Stringnumber);
+
+
