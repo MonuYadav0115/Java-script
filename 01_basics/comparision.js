756ef3c7d3d4da9b0fa70b90efa394f7d88ecf43
Monu Yadav <118626203+MonuYadav0115@users.noreply.github.com>
2025-10-14 07:25:11 +0000

    comparisioon

---
+++ 01_basics/comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 == 3);
+// console.log(2 >= 3);
+// console.log(2 <= 3);
+// console.log(2 <= 3);
+
+
+console.log("2">1);    // true 
+console.log("02">1);   // true
+
+console.log(null > 0);     // false
+console.log(null == 0);    // false
+console.log(null >= 0);    // true 
+
+// === Strict cheak 
+
+console.log("2" === 2);    // false 
+
+
+
