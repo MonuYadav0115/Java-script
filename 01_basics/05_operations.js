a3265a68761336dff03bce6f46bad8c1ed61c3a0
Monu Yadav <118626203+MonuYadav0115@users.noreply.github.com>
2025-10-14 06:58:03 +0000

    operation completed

---
+++ 01_basics/05_operations.js
@@ -0,0 +1,46 @@
+let value = 3 
+
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-1);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(12/2);
+// console.log(12%3);
+
+let str1 = "hello"
+let str2 = " Monu"
+let str3 = str1 + str2 
+
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((2+3) * 5 % 2)    ----->  // this is the right way to write a code in java script 
+
+// console.log(true)        // ----> output true 
+// console.log(+true)       // ----> output 1
+// console.log(+"")         // ----> output 0
+
+
+
+// let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter);   // output is 101
+
+// let gameCounter = 200
+// ++gameCounter;
+// console.log(gameCounter);    // output is 201
+
+
+
+
+
+
+
