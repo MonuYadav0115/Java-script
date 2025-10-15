b00fc7ae8b02ba5a0fb399964e9375fa2e6ad273
Monu Yadav <118626203+MonuYadav0115@users.noreply.github.com>
2025-10-15 06:49:15 +0000

    memory

---
+++ 01_basics/string.js
@@ -0,0 +1,12 @@
+// const name = "Monu"
+// const reponame = 20
+// console.log(name + reponame) 
+
+
+const name = "Tom Yadav"
+
+const reponame = 25
+
+console.log(`Hello my name is ${name} and my repo count is ${reponame}`);
+
+
