d3851e539f7834f27ed6e1a3d93a6e5788418db6
Monu Yadav <118626203+MonuYadav0115@users.noreply.github.com>
2025-10-16 06:48:42 +0000

    string

--- 01_basics/string.js
+++ 01_basics/string.js
@@ -1,3 +1,6 @@
+// String 
+
+
 // const name = "Monu"
 // const reponame = 20
 // console.log(name + reponame) 
@@ -7,6 +10,31 @@ const name = "Tom Yadav"
 
 const reponame = 25
 
-console.log(`Hello my name is ${name} and my repo count is ${reponame}`);
+// console.log(`Hello my name is ${name} and my repo count is ${reponame}`); 
 
+
+// const gameName = new String("pubG")
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.toUpperCase());
+// console.log(gameName.indexOf("u"));
+
+
+
+// const gameName = new String("Hello world")
+
+// const newstring = gameName.substring(0,4)
+// console.log(newstring);
+
+
+
+let Name = new String("Monu Yadav")
+
+let newString = Name.substring(0,5)
+
+console.log(newString);
 
