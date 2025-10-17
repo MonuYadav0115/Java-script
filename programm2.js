2bd190d18d9519516b6ce1021b0e81f954d0f986
Monu Yadav <monuy7883@gmail.com>
2025-10-17 14:41:20 +0530

    js

---
+++ programm2.js
@@ -0,0 +1,10 @@
+// Expand the script to include a ternary operation 
+// that checks if a user is an adult (18+) or a minor.
+
+
+let age = 29
+
+let results = (age>=18)? "Adult": "Minor";
+
+console.log(results);  // adult answer
+
