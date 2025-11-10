ede55b86a333ee9e5e25a55a57deaab4fb6b54e3
Monu Yadav <118626203+MonuYadav0115@users.noreply.github.com>
2025-11-10 18:08:02 +0000

    date

---
+++ 01_basics/date.js
@@ -0,0 +1,13 @@
+// date
+
+const myDate = new Date()
+
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+// console.log(typeof myDate)
+
+const myCreatedDate = new Date(1999, 4 , 25)
+console.log(myCreatedDate.toDateString())
+
