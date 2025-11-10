4edf2add7222e79c9fcd34e7d4667d81c412a64b
Monu Yadav <118626203+MonuYadav0115@users.noreply.github.com>
2025-11-10 18:50:41 +0000

    arrray

---
+++ 02_basics/array1.js
@@ -0,0 +1,42 @@
+// Array 
+
+const myarray = [0,1,2,3,4,5]
+
+const myHero = ["saktiman", "bahubali","Tom Cruise"]
+
+const myarray2 = new Array(1,2,3,4,5)
+
+// console.log(myarray)
+// console.log(myHero[1])
+
+// array method 
+
+// myarray.push(6)
+// myarray.push(7)
+// myarray.pop()            // its removed in last value 
+// myarray.unshift(9)      // its add first position in array 
+// myarray.shift()
+
+
+
+// console.log(myarray.includes(9))
+// console.log(myarray.indexOf(3))
+
+
+// const newarray = myarray.join()
+
+// console.log(myarray)
+// console.log(newarray)
+// console.log(typeof newarray)
+
+
+console.log("A", myarray)
+
+const myn1 = myarray.slice(1,3)
+
+console.log(myn1)
+console.log("B", myarray)
+
+const myn2 = myarray.splice(1,3)
+console.log("c", myarray)
+console.log(myn2)
