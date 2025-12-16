8bd5453f546d78ac90e69f2574c63074f69e4165
Monu Yadav <118626203+MonuYadav0115@users.noreply.github.com>
2025-12-16 17:01:13 +0000

    Some changes

--- 01_basics/datatype.js
+++ 01_basics/datatype.js
@@ -8,9 +8,9 @@ let username;
 const id = Symbol("123")
 const anotherId = Symbol("123")
 
-console.log(id === anotherId);
-console.log(typeof id)
-console.log(typeof anotherId)
+// console.log(id === anotherId);
+// console.log(typeof id)
+// console.log(typeof anotherId)
 
 
 // Non Primitive data type (Reference)
@@ -24,13 +24,53 @@ let myobj = {
 
 const myfunction = function(){
 
-    console.log("Hello world")
+    // console.log("Hello world")
 
 }
 
-console.log(typeof myfunction)       //function
-console.log(typeof myobj)           //object
-console.log(typeof heros)           //object
-console.log(typeof outsidetemp)   // object
+// console.log(typeof myfunction)       //function
+// console.log(typeof myobj)           //object
+// console.log(typeof heros)           //object
+// console.log(typeof outsidetemp)   // object
+
+// Data type december 16 
+
+// Primitive data type 
+// 7 type : String ,Number,Boolean,Null,Undefined,Symbol,Bigint
+const score1 = 100
+const scoreValue1 = 100.32
+const isloggedin1 = false
+const outsidetemp1 = null 
+let userEmail;
+const id1 = Symbol("321")
+const anotherId1 = ("321")
+
+// console.log(id1 === anotherId1)
+
+const BigNumber1 = 12345678998765432n
+
+
+// Reference Type or Non primitive type:
+
+// Array ,Object ,function --->these concept master so you achived master way in java srcipt 
+
+// array
+
+const heros1 = ["sakitiman","nagaarjun","alluarjun"]
+
+
+// objects
+
+let myobj1 = {
+    name:"Monu",
+    age1:22
+}
+
+// function
+
+const myfunction1 = function() {
+    console.log("Hello world")
+}
+myfunction1()
 
 
