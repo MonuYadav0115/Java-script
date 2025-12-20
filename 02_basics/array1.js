ac69ec85488b6d41280e94b545c04952b456ee36
Monu Yadav <118626203+MonuYadav0115@users.noreply.github.com>
2025-12-20 13:14:27 +0000

    changes

--- 02_basics/array1.js
+++ 02_basics/array1.js
@@ -1,42 +1,59 @@
-// Array 
+// // Array 
 
-const myarray = [0,1,2,3,4,5]
+// const myarray = [0,1,2,3,4,5]
 
-const myHero = ["saktiman", "bahubali","Tom Cruise"]
+// const myHero = ["saktiman", "bahubali","Tom Cruise"]
 
-const myarray2 = new Array(1,2,3,4,5)
+// const myarray2 = new Array(1,2,3,4,5)
 
-// console.log(myarray)
-// console.log(myHero[1])
+// // console.log(myarray)
+// // console.log(myHero[1])
 
-// array method 
+// // array method 
 
-// myarray.push(6)
-// myarray.push(7)
-// myarray.pop()            // its removed in last value 
-// myarray.unshift(9)      // its add first position in array 
-// myarray.shift()
-
-
-
-// console.log(myarray.includes(9))
-// console.log(myarray.indexOf(3))
+// // myarray.push(6)
+// // myarray.push(7)
+// // myarray.pop()            // its removed in last value 
+// // myarray.unshift(9)      // its add first position in array 
+// // myarray.shift()
 
 
-// const newarray = myarray.join()
 
-// console.log(myarray)
-// console.log(newarray)
-// console.log(typeof newarray)
+// // console.log(myarray.includes(9))
+// // console.log(myarray.indexOf(3))
 
 
-console.log("A", myarray)
+// // const newarray = myarray.join()
 
-const myn1 = myarray.slice(1,3)
+// // console.log(myarray)
+// // console.log(newarray)
+// // console.log(typeof newarray)
 
-console.log(myn1)
-console.log("B", myarray)
 
-const myn2 = myarray.splice(1,3)
-console.log("c", myarray)
-console.log(myn2)
+// console.log("A", myarray)
+
+// const myn1 = myarray.slice(1,3)
+
+// console.log(myn1)
+// console.log("B", myarray)
+
+// const myn2 = myarray.splice(1,3)
+// console.log("c", myarray)
+// console.log(myn2)
+
+
+// ******************* Array in second time practics *************************************
+
+
+const array = [ 2,3,4,5]
+
+const myarray = ["Monu","tom","Monutom","TomMonu"]
+
+const myarray2 = new Array(90,80,70,60)
+
+
+
+console.log(array[3])
+console.log(myarray[2])
+console.log(myarray2[2])
+
