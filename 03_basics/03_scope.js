827045cda4a94941d9695244a9c1f8eb009087a6
Monu Yadav <118626203+MonuYadav0115@users.noreply.github.com>
2025-12-26 17:30:19 +0000

    scope

--- 03_basics/03_scope.js
+++ 03_basics/03_scope.js
@@ -13,10 +13,55 @@
 
 // 2 
 
-var a = 40 
+// var a = 40 
 
-if (true){
-    var a = 50
-    console.log("Inner:a" , a)
+// if (true){
+//     var a = 50
+//     console.log("Inner:a" , a)
+// }
+// console.log(a)
+
+
+// 3 
+
+// function one (){
+//     username = "Zentrox"
+    
+//     function two(){
+//         website = "zentrox.com"
+//         console.log(username)
+//     }
+//     console.log(website)
+//     two()
+// }
+// one()
+
+
+// 4 
+
+// if (true){
+//     const username = "Tom"
+//     if (username === "Tom"){
+//         const website = "tomweb.com"
+//         console.log(username + website)
+//     }
+//     // console.log(website)
+// }
+// // console.log(username)
+
+
+
+// ********************** interesting ***********************
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
 }
-console.log(a)
+
+
+ 
+// console.log(addtwo)   // if i am run this programm show error hoisting method 
+const addtwo = function(num){
+    return num + 2
+}
+// addtwo(5)
