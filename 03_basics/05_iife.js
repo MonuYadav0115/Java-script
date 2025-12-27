4b3c0a1334d2e374eaff47cff127de54446b3174
Monu Yadav <118626203+MonuYadav0115@users.noreply.github.com>
2025-12-27 08:48:12 +0000

    js

---
+++ 03_basics/05_iife.js
@@ -0,0 +1,14 @@
+// IIFE ==> Immediatily Invoked Function Expression
+
+//named iife
+
+// (function chai (){
+//     console.log("DB connected");
+// })();
+
+// without name iife
+
+// ( (name)=>{
+//     console.log("BD connected two",name)
+// })("Tom Bravix")
+
