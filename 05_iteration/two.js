9257a1237032a0c620f156a8a465b5d90da4f50b
Monu Yadav <118626203+MonuYadav0115@users.noreply.github.com>
2026-01-01 17:21:18 +0000

    Javascript

---
+++ 05_iteration/two.js
@@ -0,0 +1,42 @@
+
+// do while loop and while loop
+
+
+// let index = 1  
+// while(index <= 10){
+//     console.log(index)
+//     index = index + 1
+// }
+
+
+
+// 2 
+
+// let myarray = ["Zentrox" , "Tom" , "Zentrox Bravix"]
+// let secarray = 0
+// while(secarray < myarray.length ){
+//     console.log(myarray[secarray])
+//     secarray = secarray + 1
+// }
+
+
+// 3 
+
+// let myarray = ["Monu" , "Zentrox" , "Tom" , "Bravix"]
+// let arr = 0
+// while(arr < myarray.length){
+//     console.log("My array is=>",myarray[arr])
+//     arr = arr + 1
+// }
+
+
+
+// do while loop
+
+let score = 11
+
+do {
+    console.log(`score is ${score}`)
+    score = score + 1
+
+} while(score <= 10)
