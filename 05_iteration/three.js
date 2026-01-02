0c4dbb4e0d86ee840bdbe437e3c07c688c31b058
Monu Yadav <118626203+MonuYadav0115@users.noreply.github.com>
2026-01-02 05:39:09 +0000

    Hello js

--- 05_iteration/three.js
+++ 05_iteration/three.js
@@ -1,14 +1,51 @@
 
 // for of 
 
-arr = [1,2,3,4,5]
+// arr = [1,2,3,4,5]
 
-for(const num of arr){
-    // console.log(num)
+// for(const num of arr){
+//      console.log(num)
+// }
+
+
+
+// const greeting = ("Hello world")
+
+// for(const greet of greeting){
+//     console.log(`iterate each word: ${greet}`)
+
+// }
+
+
+
+
+//  ********Map************** 
+
+const map = new Map()
+map.set("IN" , "India")
+map.set("USA" , "united state of america")
+map.set("FR" , "France")
+map.set("UP" , "Uttar pradesh")
+
+// console.log(map)
+
+// implement foe of loop this map 
+
+for(const [key , value] of map){
+    // console.log(key , "->" , value)
 }
 
-const greeting = ("Hello world")
 
-for(const greet of greeting){
-    console.log(greet)
+// in object to implement for of loop 
+
+const myObject = {
+    "game1": "cricket match",
+    "game2": "hockey",
+    "Game3": "basketball",
+    "game4": "Gillidandda"
+
+}
+
+for(const [key,value] of myObject){
+    console.log(key , "->" , value)       // myObject is not iterable at Object 
 }
