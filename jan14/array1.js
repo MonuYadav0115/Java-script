d7162a0c928392b406840edc11ed764eb3cef75d
Monu Tom <118626203+MonuYadav0115@users.noreply.github.com>
2026-01-14 04:25:25 +0000

    js

---
+++ jan14/array1.js
@@ -0,0 +1,92 @@
+
+// array 
+
+
+// const array = [ 1,2,3,4,5,6,8,9]
+
+// console.log(array[0])
+
+
+//
+
+// const Myhero = ["Tom Holland" , " Tom cruse" , "Zentrox bravix"]
+// console.log(Myhero)
+// console.log(Myhero.length)
+
+
+
+// 
+
+// const array = [1,2,3,4,5,6]
+
+// array.push(7)
+// console.log(array)
+
+
+
+// const array = [1,2,3,4,5,6]
+// array.pop()
+// console.log(array)
+
+//
+
+// const   array = [1,2,3,4,5,6]
+// array.unshift(1)
+// console.log(array)
+
+
+
+// const array = [1,2,3,4,5,6]
+
+// array.shift()
+// console.log(array)
+
+
+//
+
+
+// const array = [1,2,3,4,5,6,7]
+// console.log(array.includes(2))
+
+
+
+// const array = [ "tom" , "zentrox" , "holland"]
+// console.log(array.includes("monu"))
+
+
+// // 
+
+// const array = [1,2,3,4,5,6]
+
+// console.log(array.indexOf(3))
+// console.log(array.indexOf(12))
+
+
+
+
+
+
+// const array = [1,2,3,4,5,6]
+
+// const newarray = array.join()
+
+// console.log(array)
+// console.log(newarray)
+// console.log(typeof newarray)
+
+
+
+
+
+//slice
+
+// const array = [1,2,3,4,5,6]
+// let newarray = array.slice(1,3)
+// console.log(newarray)
+
+
+// splice 
+
+// const array = [1,2,3,4,5,6]
+// const newarray = array.splice(1,3)
+// console.log(newarray)
