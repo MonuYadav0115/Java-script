9016cf7ff29fbccd8552fe6d7e882f320570b34e
Monu Tom <118626203+MonuYadav0115@users.noreply.github.com>
2026-01-14 06:17:59 +0000

    js

---
+++ jan14/object2.js
@@ -0,0 +1,35 @@
+
+
+// const tinderuser = {}
+
+// tinderuser.name = "zentrox"
+// tinderuser.id = "1234@123"
+// tinderuser.email = "tom@gmail.com"
+// tinderuser.password = "!23213Aklsdls"
+
+// console.log(tinderuser)
+
+
+
+// const username ={
+//     fullname:{
+//         userfullname:{
+//             firstname:"zentrox",
+//             lastname:"tom cruse"
+//         }
+//     }
+// }
+
+// console.log(username.fullname.userfullname.lastname)
+// console.log(username.fullname.userfullname.firstname)
+
+
+
+//  const obj1 = {name:"Zentrox" , age:21 , place: "varansi" }
+//  const obj2 = {name1:"tom" , age1:22 , place1: "Hyderabd" }
+
+
+// // const obj3 = (obj1,obj2)
+
+//  const obj3 = Object.assign(obj1,obj2)
+//  console.log(obj3)
